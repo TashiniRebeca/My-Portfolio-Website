031aee99ead2fb447b8a3213467a1d9e66c4482c
Tashini Rebeca <tweerasooriya032@gmail.com>
2025-01-02 10:27:43 +0530

    updated the resume

--- components/hero.tsx
+++ components/hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
   return (
     <section ref={ref} id="home" className="scroll-mt-[100rem]">
       <motion.div
-        className="ml-8 justify-center grid grid-cols-1 sm:grid-cols-12"
+        className="grid justify-center grid-cols-1 ml-8 sm:grid-cols-12"
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{
@@ -26,8 +26,8 @@ const Hero = () => {
           duration: 0.5,
         }}
       >
-        <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+        <div className="col-span-7 text-center place-self-center sm:text-left">
+          <h1 className="mb-4 text-4xl font-extrabold text-white sm:text-5xl lg:text-6xl">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-slate-400">
               Hello, I&apos;m{" "}
             </span>
@@ -50,21 +50,21 @@ const Hero = () => {
               Hire Me <MdArrowCircleRight />
             </Link>
             <MagicBtn
-              href="/CV.pdf"
+              href="/My Resume-Tashini.pdf"
               icon={<MdOutlineFileDownload opacity={500} />}
               position="right"
               otherClasses="focus:scale-110 active:scale-100 transition hover:scale-110"
             />
           </div>
         </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+        <div className="col-span-5 mt-4 place-self-center lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/girl.jpg"
               width={400}
               height={400}
               alt="Hero Image"
-              className="ml-4 absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute ml-4 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             />
           </div>
         </div>
